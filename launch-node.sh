81d2d423b91a540d6143c80fc9477dff67560066
caelumdev <walschot.vanstalle@gmail.com>
2019-01-31 22:03:43 +0100

    Node kickstarter + config example

---
+++ launch-node.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+intexit() {
+    kill -HUP -$$
+}
+
+hupexit() {
+    echo
+    echo "Interrupted, killing daemon"
+    exit
+}
+
+trap hupexit HUP
+trap intexit INT
+
+### USAGE:
+# ./add-single-node.sh CONFIG_FILE
+
+
+touch .pwd
+export $(cat ${1} | xargs)
+
+WORK_DIR=$PWD
+cd $WORK_DIR
+
+if [ ! -d ./networks/testnet/nodes/1/caelum/chaindata ]
+then
+  wallet=$(caelum account import --password ${PASS} --datadir ${DATADIR}/${NAME} <(echo ${PRIVATE_KEY}) | awk -v FS="({|})" '{print $2}')
+  # Init our blockchain. Pay attention to using the correct version (testnet/main)
+  caelum --datadir ${DATADIR}/${NAME} init ./config/chain/testnet/genesis.json
+else
+  wallet=$(caelum account list --datadir ${DATADIR}/${NAME} | head -n 1 | awk -v FS="({|})" '{print $2}')
+fi
+
+
+echo Starting the nodes ...
+
+caelum \
+    --bootnodes "enode://f29dbe9e359e2241049951c05c54d4e01a1e65be262d70543f7e8004ef6d6449474880208d96e05a8c58009410ccc9dc0ceccb199523608e72a975d3c937d792@159.65.89.43:30301" --syncmode "full" \
+    --datadir ${DATADIR}/${NAME} --networkid 159 --port $PORT \
+    --unlock "${wallet}" --ethstats "$NAME:caelumTestNet@159.65.89.43:3004" \
+    --password ${PASS} --mine &
+
+wait
