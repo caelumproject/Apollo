172d22cb2699376cdd3a636a19a9259467268e70
caelumdev <walschot.vanstalle@gmail.com>
2019-03-28 18:41:31 +0100

    Fix stats IP

--- launch-node.sh
+++ launch-node.sh
@@ -39,7 +39,7 @@ echo Starting the nodes for ${wallet}...
 caelum \
     --bootnodes "enode://fea3712d633300fc5c3ee9afbebc39e22245ee70592afd84b20778eff6fe3d71351ee7bafddb7dca3535c798a8fb4dd0c912848682579530483b638521e6f311@104.248.93.200:30301,enode://2aa2134aa2a1df5b9fd23e8df5bab6b57ce59148434fb42f2dd154a3fd9de182d65963dc7faecb16c4483f18d608f3740dd3783a8e7bb61c4ade8d25ca72fbf1@104.248.93.200:30344" --syncmode "full" \
     --datadir ${DATADIR}/${NAME} --networkid 159 --port $PORT \
-    --unlock "${wallet}" --ethstats "$NAME:caelumTestNet@159.65.89.43:3004" \
+    --unlock "${wallet}" --ethstats "$NAME:caelumTestNet@206.189.241.127:3004" \
     --password ./.pwd  --syncmode "full" &
 
 wait
